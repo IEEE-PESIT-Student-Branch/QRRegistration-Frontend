2aaa85a1b85629fcd5aee918487806d1625fb5b5
Aravindhan V <31785578+AravindhanV@users.noreply.github.com>
2020-02-10 17:42:30 +0530

    Cleaned project structure (#3)

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:ieee_qr_app/main.dart';
+import 'package:ieee/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
